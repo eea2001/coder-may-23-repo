622fd9751f0a8e38f97c2a9c228dc7757509cefd
ellise <128885786+eea2001@users.noreply.github.com>
2023-06-10 00:34:46 +1000

    python, madlibs game

---
+++ python/9june23.py
@@ -0,0 +1,22 @@
+from math import * 
+#link break \n
+print("Giraffe\nAcademy")
+
+phrase = "Giraffe Academy"
+print(phrase)
+
+#concatenation, appending strings 
+print(phrase +" is cool")
+
+#functions to modify or get info of strings 
+print(phrase.upper())
+print(phrase.isupper())
+
+print(phrase.upper().isupper())
+
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print("Hello " + name + "! You are " + age)
+
+num1 = input("Enter a number:")
